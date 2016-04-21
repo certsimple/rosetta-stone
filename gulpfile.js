1733f49e3a8a6dfd6a716287b4f591c7d684f30f
Aaron Miller <me@aaron-miller.me>
2016-04-21 12:47:41 -0400

    Listen on all interfaces, not just lo

--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,7 @@ gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
       livereload: true,
+      host: '0.0.0.0',
       port: 7777,
       fallback: 'html/index.html',
       open: true
